bd9d2c866a943e5211dfc4bdf16f4c403a759982
Петров Илья Вадимович <ipb.tech@yandex.ru>
2025-01-24 23:58:12 +0300

    simple client

---
+++ ChuckNorris.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using ChuckNorris.Client;
+using Grpc.Net.Client;
+
+Console.WriteLine("Let's fun with Chuck Norris");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5144");
+var client = new ChuckNorris.Client.ChuckNorris.ChuckNorrisClient(channel);
+
+for (int i = 0; i < 10; i++)
+{
+    var joke = await client.ToJokeAsync(new EmptyRequest());
+    Console.WriteLine(joke.Value);
+    Console.WriteLine();
+}
+Console.ReadLine();
